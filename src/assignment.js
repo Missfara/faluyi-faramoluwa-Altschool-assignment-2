9d0f5d6ef27df80bb253dbfa02582399aac8c697
Faluyi Faramoluwa <faluyi94@gmail.com>
2022-07-07 12:33:25 +0000

    challenge 1 and 2 attempted

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,18 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  return arrayOfNumbers.reduce(function (prev, current){
+return (prev+current)
+  }
+  )                             
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+                                                                                          
+ assignment.sumOfNumbers = sumOfNumbers;
+
+
+
+
+
 
 /**
  * Challenge - 2
@@ -28,9 +37,12 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  return arrayOfNumbers.filter(num=> num%2==0).length  
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+assignment.countEvenNumbers = countEvenNumbers;
+
+
 
 /**
  * Challenge - 3
